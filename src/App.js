229104c85a82484fc01bbed345d1df64dce0d627
George Baraza <barazageorge16@gmail.com>
2024-03-03 03:28:18 +0300

    New Repo here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/home';
+import About from './pages/about';
+import Blog from './pages/blog';
+import Contacts from './pages/contacts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Blog</h1>
+      <Home />
+      <About />
+      <Blog />
+      <Contacts />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h3>Welcome to My Blog</h3>
+      <p>Hello there.</p>
+    </div>
+  );
+}
+
+export default Home;
+  
